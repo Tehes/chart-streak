a89ddb2d21858844d1c6bfe0556861cb6305e981
Tino Sabetta <tino.sabetta@gmail.com>
2026-01-11 16:13:40 +0100

    updated data for 1983

--- tools/check-deezer-readable.js
+++ tools/check-deezer-readable.js
@@ -1,6 +1,6 @@
 const enrichedDirUrl = new URL("../data/enriched/", import.meta.url);
 const delayMs = 2000;
-const defaultYear = "1982";
+const defaultYear = "1983";
 const textEncoder = new TextEncoder();
 
 function getInputFileUrl(input) {
